3b3aefb7fbf15ee83936d0120acfab9ddc5be58d
Tony Hirst <tony.hirst@gmail.com>
2020-05-19 10:18:22 +0100

    Update __init__.py

--- jp_flowchartjs/__init__.py
+++ jp_flowchartjs/__init__.py
@@ -1,1 +1,6 @@
+from IPython.core.magic import register_cell_magic
 
+@register_cell_magic
+def flowchart_magic(line, cell):
+    "Send code to simulator."
+    return FlowchartWidget().charter(cell, embed=True)
