d0e871e0231387a8c6768d65dc6b57d78c8f191d
Tony Hirst <tony.hirst@gmail.com>
2020-11-02 18:46:13 +0000

    Setup / version for pypi

--- jp_flowchartjs/__init__.py
+++ jp_flowchartjs/__init__.py
@@ -1,6 +1,8 @@
 from IPython.core.magic import register_cell_magic
 from jp_flowchartjs.jp_flowchartjs import FlowchartWidget
 
+__version__="0.0.1"
+
 @register_cell_magic
 def flowchart_magic(line, cell):
     "Send code to simulator."
--- setup.py
+++ setup.py
@@ -1,7 +1,58 @@
   
 from setuptools import setup
 
+from os import path
+
+def get_long_description():
+    with open(
+        path.join(path.dirname(path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
+
+def get_requirements(fn='requirements.txt', nogit=True):
+   """Get requirements."""
+   if path.exists(fn):
+      with open(fn, 'r') as f:
+        requirements = f.read().splitlines()
+   else:
+     requirements = []
+   requirements = [r.split()[0].strip() for r in requirements if r and not r.startswith('#')]
+   if nogit:
+       requirements = [r for r in requirements if not r.startswith('git+')]
+   return requirements
+
+requirements = get_requirements()
+
+print(f'Requirements: {requirements}')
+
 setup(name='jp_flowchartjs',
-      install_requires=['jp_flowchartjs'],
-      packages=['jp_flowchartjs']
+    author='Tony Hirst',
+    author_email='tony.hirst@open.ac.uk',
+    url='https://github.com/innovationOUtside/flowchart_js_jp_proxy_widget',
+    description='IPython/Jupyter notebook magic for generating JS flowcharts.',
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    license='MIT License',
+    packages=['jp_flowchartjs'],
+    # Dependencies
+    install_requires=requirements,
+    #setup_requires=[],
+    
+    # Packaging
+    #entry_points="",
+    include_package_data=True,
+    zip_safe=False,
+
+    # Classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Education',
+        'License :: Free For Educational Use',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Education',
+        'Topic :: Scientific/Engineering :: Visualization'
+    ],
 )
